83e2a6f402d69fc85cd62163a8ca8e2ab2b09e27
Sakti Dwi Cahyono <54krpl@gmail.com>
2013-02-18 16:45:09 +0700

    add script id and description

--- sscript/nmap.py
+++ sscript/nmap.py
@@ -2,6 +2,11 @@
 from sh import nmap
 
 
+__id__ = 2
+__author__ = 'Sakti Dwi Cahyono'
+__desc__ = 'Look for open port on target host'
+
+
 def run(url):
     result = nmap(url.netloc)
 
--- sscript/robotstxt.py
+++ sscript/robotstxt.py
@@ -2,6 +2,11 @@
 import requests
 
 
+__id__ = 2
+__author__ = 'Sakti Dwi Cahyono'
+__desc__ = 'Check if robots.txt contain secret data'
+
+
 def run(url):
     robots_url = '%s://%s/robots.txt' % (url.scheme, url.netloc)
     result = requests.get(robots_url)
